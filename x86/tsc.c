sean-jc/kvm-unit-tests

commit 10631a5bebd8bafe87646b73afd24125cb77abef

x86: tsc: add rdpid test
Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

x86/tsc.c
@@ -7,6 +7,11 @@ int check_cpuid_80000001_edx(unsigned int bit)
     return (cpuid(0x80000001).d & bit) != 0;
 }
 
+#define CPUID_7_0_ECX_RDPID		    (1 << 22)
+int check_cpuid_7_0_ecx(unsigned int bit)
+{
+    return (cpuid_indexed(7, 0).c & bit) != 0;
+}
 
 void test_wrtsc(u64 t1)
 {
@@ -26,6 +31,15 @@ void test_rdtscp(u64 aux)
        report("Test RDTSCP %" PRIu64, ecx == aux, aux);
 }
 
+void test_rdpid(u64 aux)
+{
+       u32 eax;
+
+       wrmsr(MSR_TSC_AUX, aux);
+       asm (".byte 0xf3, 0x0f, 0xc7, 0xf8" : "=a" (eax));
+       report("Test rdpid %%eax %d", eax == aux, aux);
+}
+
 int main()
 {
 	u64 t1, t2;
@@ -43,5 +57,12 @@ int main()
 		test_rdtscp(0x100);
 	} else
 		printf("rdtscp not supported\n");
+
+	if (check_cpuid_7_0_ecx(CPUID_7_0_ECX_RDPID)) {
+		test_rdpid(0);
+		test_rdpid(10);
+		test_rdpid(0x100);
+	} else
+		printf("rdpid not supported\n");
 	return report_summary();
 }

commit 30e4e6d72dfcddea1ce72ff5117c844dbba790d8

x86: tsc: use the correct print format to fix a build warning in test_rdpid
Fixes: 10631a5 (x86: tsc: add rdpid test)
Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>

x86/tsc.c
@@ -37,7 +37,7 @@ void test_rdpid(u64 aux)
 
        wrmsr(MSR_TSC_AUX, aux);
        asm (".byte 0xf3, 0x0f, 0xc7, 0xf8" : "=a" (eax));
-       report("Test rdpid %%eax %d", eax == aux, aux);
+       report("Test rdpid %%eax %" PRIu64, eax == aux, aux);
 }
 
 int main(void)