sean-jc/kvm-unit-tests

commit 03b1e4570f9678c59a1fdcd0428d21681541602e

x86: Support environments without test-devices
Enable to run the tests when test-device is not present (e.g.,
bare-metal). Users can provide the number of CPUs and ram size through
kernel parameters.

On Ubuntu that uses grub, for example, the tests can be run by copying a
test to the boot directory (/boot) and adding a menu-entry to grub
(e.g., by editing /etc/grub.d/40_custom):

  menuentry 'idt_test' {
	set root='[ROOT]'
	multiboot [BOOT_RELATIVE]/[TEST].flat [PARAMETERS]
	module params.initrd
  }

Replace:
  * [ROOT] with `grub-probe --target=bios_hints /boot`
  * [BOOT_RELATIVE] with `grub-mkrelpath /boot`
  * [TEST] with the test executed
  * [PARAMETERS] with the test parameters

params.initrd, which would be located on the boot directory should
describe the machine and tell the test infrastructure that a test
device is not present and boot-loader was used (the bootloader and qemu
deliver test . For example for a 4 core machines with 4GB of
memory:

  NR_CPUS=4
  MEMSIZE=4096
  TEST_DEVICE=0
  BOOTLOADER=1

Since we do not really use E820, using more than 4GB is likely to fail
due to holes.

Finally, do not forget to run update-grub. Remember that the output goes
to the serial port.

Cc: Andrew Jones <drjones@redhat.com>
Signed-off-by: Nadav Amit <nadav.amit@gmail.com>
Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

lib/x86/fwcfg.c
@@ -1,13 +1,41 @@
 #include "fwcfg.h"
 #include "smp.h"
+#include "libcflat.h"
 
 static struct spinlock lock;
+
+static long fw_override[FW_CFG_MAX_ENTRY];
+
+bool no_test_device;
+
+void read_cfg_override(void)
+{
+	const char *str;
+	int i;
+
+	/* Initialize to negative value that would be considered as invalid */
+	for (i = 0; i < FW_CFG_MAX_ENTRY; i++)
+		fw_override[i] = -1;
+
+	if ((str = getenv("NR_CPUS")))
+		fw_override[FW_CFG_NB_CPUS] = atol(str);
+
+	/* MEMSIZE is in megabytes */
+	if ((str = getenv("MEMSIZE")))
+		fw_override[FW_CFG_RAM_SIZE] = atol(str) * 1024 * 1024;
+
+	if ((str = getenv("TEST_DEVICE")))
+		no_test_device = !atol(str);
+}
 
 static uint64_t fwcfg_get_u(uint16_t index, int bytes)
 {
     uint64_t r = 0;
     uint8_t b;
     int i;
+
+    if (fw_override[index] >= 0)
+	    return fw_override[index];
 
     spin_lock(&lock);
     asm volatile ("out %0, %1" : : "a"(index), "d"((uint16_t)BIOS_CFG_IOPORT));

lib/x86/fwcfg.h
@@ -2,6 +2,7 @@
 #define FWCFG_H
 
 #include <stdint.h>
+#include <stdbool.h>
 
 #define FW_CFG_SIGNATURE        0x00
 #define FW_CFG_ID               0x01
@@ -32,6 +33,15 @@
 #define FW_CFG_ACPI_TABLES (FW_CFG_ARCH_LOCAL + 0)
 #define FW_CFG_SMBIOS_ENTRIES (FW_CFG_ARCH_LOCAL + 1)
 #define FW_CFG_IRQ0_OVERRIDE (FW_CFG_ARCH_LOCAL + 2)
+
+extern bool no_test_device;
+
+void read_cfg_override(void);
+
+static inline bool test_device_enabled(void)
+{
+	return !no_test_device;
+}
 
 uint8_t fwcfg_get_u8(unsigned index);
 uint16_t fwcfg_get_u16(unsigned index);

x86/cstart64.S
@@ -246,8 +246,6 @@ start64:
 	call mask_pic_interrupts
 	call enable_apic
 	call save_id
-	call smp_init
-	call enable_x2apic
 	mov mb_boot_info(%rip), %rbx
 	mov %rbx, %rdi
 	call setup_multiboot
@@ -255,6 +253,12 @@ start64:
 	mov mb_cmdline(%rbx), %eax
 	mov %rax, __args(%rip)
 	call __setup_args
+
+	/* Read the configuration before running smp_init */
+	call read_cfg_override
+	call smp_init
+	call enable_x2apic
+
 	mov __argc(%rip), %edi
 	lea __argv(%rip), %rsi
 	lea __environ(%rip), %rdx

lib/argv.c
@@ -53,12 +53,17 @@ static void setup_args(const char *args)
 	__setup_args();
 }
 
+void add_setup_arg(const char *arg)
+{
+	__argv[__argc] = copy_ptr;
+	strcpy(__argv[__argc], arg);
+	copy_ptr += strlen(arg) + 1;
+	++__argc;
+}
+
 void setup_args_progname(const char *args)
 {
-	__argv[0] = copy_ptr;
-	strcpy(__argv[0], auxinfo.progname);
-	copy_ptr += strlen(auxinfo.progname) + 1;
-	++__argc;
+	add_setup_arg(auxinfo.progname);
 	setup_args(args);
 }
 

lib/argv.h
@@ -8,3 +8,4 @@
 extern void __setup_args(void);
 extern void setup_args_progname(const char *args);
 extern void setup_env(char *env, int size);
+extern void add_setup_arg(const char *arg);

lib/x86/setup.c
@@ -8,6 +8,7 @@
 #include "libcflat.h"
 #include "fwcfg.h"
 #include "alloc_phys.h"
+#include "argv.h"
 
 extern char edata;
 
@@ -66,7 +67,11 @@ void setup_libcflat(void)
 	if (initrd) {
 		/* environ is currently the only file in the initrd */
 		u32 size = MIN(initrd_size, ENV_SIZE);
+		const char *str;
+
 		memcpy(env, initrd, size);
 		setup_env(env, size);
+		if ((str = getenv("BOOTLOADER")) && atol(str) != 0)
+			add_setup_arg("bootloader");
 	}
 }

x86/apic.c
@@ -6,6 +6,7 @@
 #include "isr.h"
 #include "msr.h"
 #include "atomic.h"
+#include "fwcfg.h"
 
 #define MAX_TPR			0xf
 
@@ -655,7 +656,8 @@ int main(void)
 
     test_self_ipi();
     test_physical_broadcast();
-    test_pv_ipi();
+    if (test_device_enabled())
+        test_pv_ipi();
 
     test_sti_nmi();
     test_multiple_nmi();

x86/eventinj.c
@@ -8,6 +8,7 @@
 #include "vmalloc.h"
 #include "alloc_page.h"
 #include "delay.h"
+#include "fwcfg.h"
 
 #ifdef __x86_64__
 #  define R "r"
@@ -28,7 +29,11 @@ static void apic_self_nmi(void)
 	apic_icr_write(APIC_DEST_PHYSICAL | APIC_DM_NMI | APIC_INT_ASSERT, 0);
 }
 
-#define flush_phys_addr(__s) outl(__s, 0xe4)
+#define flush_phys_addr(__s) do {					\
+		if (test_device_enabled())				\
+			outl(__s, 0xe4);				\
+	} while (0)
+
 #define flush_stack() do {						\
 		int __l;						\
 		flush_phys_addr(virt_to_phys(&__l));			\
@@ -136,6 +141,8 @@ extern void do_iret(ulong phys_stack, void *virt_stack);
 // Return to same privilege level won't pop SS or SP, so
 // save it in RDX while we run on the nested stack
 
+extern bool no_test_device;
+
 asm("do_iret:"
 #ifdef __x86_64__
 	"mov %rdi, %rax \n\t"		// phys_stack
@@ -148,10 +155,14 @@ asm("do_iret:"
 	"pushf"W" \n\t"
 	"mov %cs, %ecx \n\t"
 	"push"W" %"R "cx \n\t"
-	"push"W" $1f \n\t"
+	"push"W" $2f \n\t"
+
+	"cmpb $0, no_test_device\n\t"	// see if need to flush
+	"jnz 1f\n\t"
 	"outl %eax, $0xe4 \n\t"		// flush page
+	"1: \n\t"
 	"iret"W" \n\t"
-	"1: xchg %"R "dx, %"R "sp \n\t"	// point to old stack
+	"2: xchg %"R "dx, %"R "sp \n\t"	// point to old stack
 	"ret\n\t"
    );
 

commit 986bcf395bd7454094f7979d9508a1933e09bd59

x86: Fix breakage of fw_cfg for 32-bit unit tests
Ensure the fw_cfg overrides are parsed prior consuming any of said
overrides.  fwcfg_get_u() treats zero as a valid overide value, which
is slightly problematic since the overrides are in the .bss and thus
initialized to zero.

Add a limit check when indexing fw_override so that future code doesn't
spontaneously explode.

Cc: Nadav Amit <nadav.amit@gmail.com>
Fixes: 03b1e4570f967 ("x86: Support environments without test-devices")
Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>

lib/x86/fwcfg.c
@@ -5,10 +5,11 @@
 static struct spinlock lock;
 
 static long fw_override[FW_CFG_MAX_ENTRY];
+static bool fw_override_done;
 
 bool no_test_device;
 
-void read_cfg_override(void)
+static void read_cfg_override(void)
 {
 	const char *str;
 	int i;
@@ -26,6 +27,8 @@ void read_cfg_override(void)
 
 	if ((str = getenv("TEST_DEVICE")))
 		no_test_device = !atol(str);
+
+    fw_override_done = true;
 }
 
 static uint64_t fwcfg_get_u(uint16_t index, int bytes)
@@ -34,7 +37,10 @@ static uint64_t fwcfg_get_u(uint16_t index, int bytes)
     uint8_t b;
     int i;
 
-    if (fw_override[index] >= 0)
+    if (!fw_override_done)
+        read_cfg_override();
+
+    if (index < FW_CFG_MAX_ENTRY && fw_override[index] >= 0)
 	    return fw_override[index];
 
     spin_lock(&lock);

lib/x86/fwcfg.h
@@ -36,8 +36,6 @@
 
 extern bool no_test_device;
 
-void read_cfg_override(void);
-
 static inline bool test_device_enabled(void)
 {
 	return !no_test_device;

x86/cstart64.S
@@ -254,8 +254,6 @@ start64:
 	mov %rax, __args(%rip)
 	call __setup_args
 
-	/* Read the configuration before running smp_init */
-	call read_cfg_override
 	call smp_init
 	call enable_x2apic
 