sean-jc/kvm-unit-tests

commit dcf27dc5b5499bd35e5a19b68cf4fc692a73e796

x86: Fix the logical destination mode test
There are following issues with the ioapic logical destination mode test:

- A race condition that is triggered when the interrupt handler
  ioapic_isr_86() is called at the same time by multiple vCPUs. Due to this
  the g_isr_86 is not correctly incremented. To prevent this a spinlock is
  added around ‘g_isr_86++’.

- On older QEMU versions initial x2APIC ID is not set, that is why
  the local APIC IDs of each vCPUs are not configured. Hence the logical
  destination mode test fails/hangs. Adding ‘+x2apic’ to the qemu -cpu params
  ensures that the local APICs are configured every time, irrespective of the
  QEMU version.

- With ‘-machine kernel_irqchip=split’ included in the ioapic test
  test_ioapic_self_reconfigure() always fails and somehow leads to a state where
  after submitting IOAPIC fixed delivery - logical destination mode request we
  never receive an interrupt back. For now, the physical and logical destination
  mode tests are moved above test_ioapic_self_reconfigure().

Fixes: b2a1ee7e ("kvm-unit-test: x86: ioapic: Test physical and logical destination mode")
Signed-off-by: Nitesh Narayan Lal <nitesh@redhat.com>
Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

x86/ioapic.c
@@ -432,10 +432,13 @@ static void test_ioapic_physical_destination_mode(void)
 }
 
 static volatile int g_isr_86;
+struct spinlock ioapic_lock;
 
 static void ioapic_isr_86(isr_regs_t *regs)
 {
+	spin_lock(&ioapic_lock);
 	++g_isr_86;
+	spin_unlock(&ioapic_lock);
 	set_irq_line(0x0e, 0);
 	eoi();
 }
@@ -501,6 +504,10 @@ int main(void)
 	test_ioapic_level_tmr(true);
 	test_ioapic_edge_tmr(true);
 
+	test_ioapic_physical_destination_mode();
+	if (cpu_count() > 3)
+		test_ioapic_logical_destination_mode();
+
 	if (cpu_count() > 1) {
 		test_ioapic_edge_tmr_smp(false);
 		test_ioapic_level_tmr_smp(false);
@@ -508,11 +515,7 @@ int main(void)
 		test_ioapic_edge_tmr_smp(true);
 
 		test_ioapic_self_reconfigure();
-		test_ioapic_physical_destination_mode();
 	}
-
-	if (cpu_count() > 3)
-		test_ioapic_logical_destination_mode();
 
 	return report_summary();
 }

x86/unittests.cfg
@@ -46,7 +46,7 @@ timeout = 30
 [ioapic]
 file = ioapic.flat
 smp = 4
-extra_params = -cpu qemu64
+extra_params = -cpu qemu64,+x2apic
 arch = x86_64
 
 [cmpxchg8b]

commit e7fd658575ba5570e5a9e976f4656c49be46b34d

x86: ioapic: Run physical destination mode test iff cpu_count() > 1
Make test_ioapic_physical_destination_mode() depending on having at
least two CPUs as it sets ->dest_id to '1', i.e. expects CPU0 and CPU1
to exist.  This analysis is backed up by the fact that the test was
originally gated by cpu_count() > 1.

Fixes: dcf27dc5b5499 ("x86: Fix the logical destination mode test")
Cc: Nitesh Narayan Lal <nitesh@redhat.com>
Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>

x86/ioapic.c
@@ -504,7 +504,8 @@ int main(void)
 	test_ioapic_level_tmr(true);
 	test_ioapic_edge_tmr(true);
 
-	test_ioapic_physical_destination_mode();
+	if (cpu_count() > 1)
+		test_ioapic_physical_destination_mode();
 	if (cpu_count() > 3)
 		test_ioapic_logical_destination_mode();
 