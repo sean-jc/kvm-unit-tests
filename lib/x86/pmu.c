0e86b18ea0e4dbb0445bb1a81dd381a777f7b6e0
Like Xu <likexu@tencent.com>
2022-10-24 17:12:23 +0800

    x86/pmu: Add AMD Guest PerfMonV2 testcases
    Updated test cases to cover KVM enabling code for AMD Guest PerfMonV2.
    
    The Intel-specific PMU helpers were added to check for AMD cpuid, and
    some of the same semantics of MSRs were assigned during the initialization
    phase. The vast majority of pmu test cases are reused seamlessly.
    
    On some x86 machines (AMD only), even with retired events, the same
    workload is measured repeatedly and the number of events collected is
    erratic, which essentially reflects the details of hardware implementation,
    and from a software perspective, the type of event is an unprecise event,
    which brings a tolerance check in the counter overflow testcases.
    
    Signed-off-by: Like Xu <likexu@tencent.com>
    Signed-off-by: Sean Christopherson <seanjc@google.com>

--- lib/x86/pmu.c
+++ lib/x86/pmu.c
@@ -39,9 +39,15 @@ void pmu_init(void)
 			pmu.msr_global_status_clr = MSR_CORE_PERF_GLOBAL_OVF_CTRL;
 		}
 	} else {
+		/* Performance Monitoring Version 2 Supported */
+		if (this_cpu_has(X86_FEATURE_AMD_PMU_V2))
+			pmu.version = 2;
+
 		pmu.msr_gp_counter_base = MSR_F15H_PERF_CTR0;
 		pmu.msr_gp_event_select_base = MSR_F15H_PERF_CTL0;
-		if (!this_cpu_has(X86_FEATURE_PERFCTR_CORE))
+		if (this_cpu_has(X86_FEATURE_AMD_PMU_V2))
+			pmu.nr_gp_counters = cpuid(0x80000022).b & 0xf;
+		else if (!this_cpu_has(X86_FEATURE_PERFCTR_CORE))
 			pmu.nr_gp_counters = AMD64_NUM_COUNTERS;
 		else
 			pmu.nr_gp_counters = AMD64_NUM_COUNTERS_CORE;
@@ -49,6 +55,12 @@ void pmu_init(void)
 		pmu.gp_counter_width = PMC_DEFAULT_WIDTH;
 		pmu.gp_counter_mask_length = pmu.nr_gp_counters;
 		pmu.gp_counter_available = (1u << pmu.nr_gp_counters) - 1;
+
+		if (this_cpu_has_perf_global_status()) {
+			pmu.msr_global_status = MSR_AMD64_PERF_CNTR_GLOBAL_STATUS;
+			pmu.msr_global_ctl = MSR_AMD64_PERF_CNTR_GLOBAL_CTL;
+			pmu.msr_global_status_clr = MSR_AMD64_PERF_CNTR_GLOBAL_STATUS_CLR;
+		}
 	}
 
 	pmu_reset_all_counters();
