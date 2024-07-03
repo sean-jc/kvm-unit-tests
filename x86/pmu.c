438961f91a6b224d376e3ed07d22a9a52a2348fa
Dapeng Mi <dapeng1.mi@linux.intel.com>
2024-07-03 09:56:59 +0000

    x86: pmu: Enlarge cnt[] length to 48 in check_counters_many()
    Considering there are already 8 GP counters and 4 fixed counters on
    latest Intel processors, like Sapphire Rapids. The original cnt[] array
    length 10 is definitely not enough to cover all supported PMU counters on
    these new processors even through currently KVM only supports 3 fixed
    counters at most. This would cause out of bound memory access and may trigger
    false alarm on PMU counter validation
    
    It's probably more and more GP and fixed counters are introduced in the
    future and then directly extends the cnt[] array length to 48 once and
    for all. Base on the layout of IA32_PERF_GLOBAL_CTRL and
    IA32_PERF_GLOBAL_STATUS, 48 looks enough in near feature.
    
    Reviewed-by: Jim Mattson <jmattson@google.com>
    Signed-off-by: Dapeng Mi <dapeng1.mi@linux.intel.com>
    Signed-off-by: Sean Christopherson <seanjc@google.com>

--- x86/pmu.c
+++ x86/pmu.c
@@ -255,7 +255,7 @@ static void check_fixed_counters(void)
 
 static void check_counters_many(void)
 {
-	pmu_counter_t cnt[10];
+	pmu_counter_t cnt[48];
 	int i, n;
 
 	for (i = 0, n = 0; n < pmu.nr_gp_counters; i++) {
